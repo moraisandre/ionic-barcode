9bec0e0d2868fd821d7cdd97a79607ff661003f9
Andre Morais <andre.morais@sinqia.com.br>
2020-01-08 00:43:11 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  label = 'Teste';
+  formato = 'FORMATO';
 
+  constructor(
+    public navCtrl: NavController,
+    private barcodeScanner: BarcodeScanner
+  ) {}
+
+  scan() {
+
+    //ToDO: use later
+    const options = {
+      format: 'CODE_39'
+    };
+
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        console.log("Barcode data", barcodeData);
+        this.label = barcodeData.text;
+        this.formato = barcodeData.format;
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
   }
-
 }
